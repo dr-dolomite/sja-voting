2ff04a19df0cda5a5189c0dae5b3fac52b5c4c22
Russel Yasol <russel.yasol@wvsu.edu.ph>
2026-02-22 14:26:20 +0800

    Refactor code structure for improved readability and maintainability

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,62 @@
+"use client";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
+
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+    <div className="container mx-auto flex justify-center min-h-screen">
+      <div className="grid place-items-center lg:max-w-7xl gap-8 mx-3 md:mx-auto">
+        <div className="text-center space-y-6">
+          <Image
+            src="/sja-logo.webp"
+            alt="SJA Voting System Logo"
+            width={192}
+            height={192}
+            className="mx-auto"
+          />
+
+          <div className="max-w-6xl mx-auto text-center text-4xl md:text-7xl font-bold">
+            <h1>
+              Choose your next leaders with
+              <span className="text-transparent px-2 bg-linear-to-r from-primary bg-clip-text">
+                SJA Voting System
+              </span>
+            </h1>
+          </div>
+
+          <p className="max-w-3xl mx-auto text-xl text-muted-foreground">
+            A modern and secure voting system built by St. Joseph&#39;s Academy
+            of Malinao, designed to empower students in selecting their leaders
+            with confidence and ease.
           </p>
+
+          <div>
+            <Link href="/vote/login">
+              <Button
+                className="w-5/6 md:w-1/4 font-bold group/arrow"
+                size="lg"
+              >
+                Get Started
+                <ArrowRight className="size-5 group-hover/arrow:translate-x-1 transition-transform" />
+              </Button>
+            </Link>
+          </div>
+
+          <div className="grid gap-1">
+            <p className="text-muted-foreground font-medium">
+              Developed by of St. Joseph&#39;s Academy of Malinao.
+            </p>
+            <p className="text-sm text-muted-foreground">
+              &copy; {new Date().getFullYear()} St. Joseph&#39;s Academy of
+              Malinao, Inc. All rights reserved.
+            </p>
+          </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+      </div>
     </div>
   );
 }
